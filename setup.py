2a63a9d8ba6e844dfa14509d35c34d00e7d1d009
5hayanB <baig.sh@tutanota.com>
2024-03-13 19:32:52 +0500

    fixed setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "evaluate_functional_correctness = verilog_eval.evaluate_functional_correctness",
+            "evaluate_functional_correctness = verilog_eval.evaluate_functional_correctness:main",
         ]
     }
 )
